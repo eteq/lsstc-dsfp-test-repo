5c1eab5bef1e12d6bff9d18567f21ceab2204bcc
Erik Tollerud <erik.tollerud@gmail.com>
2017-04-25 09:38:19 -0700

    added base package

---
+++ brooks/core.py
@@ -0,0 +1,59 @@
+import sys
+import random
+
+_LOCKED = False  # this seems secure, right?
+
+
+class LockedError(Exception): pass
+
+
+def lock():
+    global _LOCKED
+
+    _LOCKED = '12345'
+
+
+def unlock(code):
+    global _LOCKED
+
+    if _LOCKED:
+        if str(code) == _LOCKED:
+            _LOCKED = False
+        else:
+            raise ValueError('Wrong code')
+
+
+def _check_lock():
+    if _LOCKED:
+        raise LockedError("The air shield is up!")
+
+
+def self_destruct():
+    _check_lock()
+    print("Thank you for pressing the self destruct button!")
+    sys.exit(0)
+
+
+_TOWNS_GRABBED = []
+def land_grab(town, black_sheriff=False):
+    _check_lock()
+
+    if black_sheriff:
+        threshold = .01
+    else:
+        threshold = .5
+
+    success = random.random() < threshold
+
+    _TOWNS_GRABBED.append(town)
+
+    if success:
+        print("Go do that voodoo that you do... so well!")
+    else:
+        print("To tell the truth, I'm getting a bit bored...")
+
+    return success
+
+
+def towns():
+    return tuple(_TOWNS_GRABBED)
