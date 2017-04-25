8e46e8af02133581a1afc09bfc62f0f964b22de3
Erik Tollerud <erik.tollerud@gmail.com>
2017-04-25 09:53:48 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='brooks',
+      version='0.1dev',
+      description='A package called brooks, but most people call it... brooks',
+      author='Erik Tollerud',
+      author_email='erik.tollerud@gmail.com',
+      packages=['brooks'],
+     )
