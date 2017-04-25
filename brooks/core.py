7fbb564f62d3cf680cdd19f43106fb32c7fb3fcc
Erik Tollerud <erik.tollerud@gmail.com>
2017-04-25 13:46:45 -0700

    docstring improvements

--- brooks/core.py
+++ brooks/core.py
@@ -8,6 +8,9 @@ class LockedError(Exception): pass
 
 
 def lock():
+    """
+    Lock the air shield!
+    """
     global _LOCKED
 
     _LOCKED = '12345'
@@ -16,6 +19,11 @@ def lock():
 def unlock(code):
     """
     It should be the same combination as my luggage...
+
+    Parameters
+    ----------
+    code
+        The unlock code to try to use to unlock
     """
     global _LOCKED
 
@@ -32,13 +40,31 @@ def _check_lock():
 
 
 def self_destruct():
+    """
+    The final win of the up side of the Schwartz against Dark Helmet...
+    """
     _check_lock()
     print("Thank you for pressing the self destruct button!")
     sys.exit(0)
 
 
 _TOWNS_GRABBED = []
-def land_grab(town='Rock Ridge', sheriff_bart=True):
+def land_snatch(town='Rock Ridge', sheriff_bart=True):
+    """
+    Land snatching!
+
+    Parameters
+    ----------
+    town : str
+        The town we want
+    sheriff_bart : bool
+        Whether or not Bart is the sheriff_bart
+
+    Returns
+    -------
+    bool
+        Whether or not the snatch was sucessful
+    """
     _check_lock()
 
     if sheriff_bart:
@@ -59,4 +85,12 @@ def land_grab(town='Rock Ridge', sheriff_bart=True):
 
 
 def towns():
+    """
+    Get the towns that have been snatched.
+
+    Returns
+    -------
+    tuple of strings
+        The names of all the towns that have been snatched.
+    """
     return tuple(_TOWNS_GRABBED)
